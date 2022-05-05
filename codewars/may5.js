ec7c9cbf152600d7fe0cd0694650b5080a971a51
Ngando Ntone <ngandontone33@gmail.com>
2022-05-05 11:36:08 -0500

    2nd problem

--- codewars/may5.js
+++ codewars/may5.js
@@ -1,3 +1,13 @@
+//1
 function opposite(number) {
   return number > 0 ? -Math.abs(number) : Math.abs(number);
+}
+//2
+function monkeyCount(n) {
+  // your code here
+  const array = [];
+  for (let i = 1; i <= n; i++) {
+    array.push(i);
+  }
+  return array;
 }
