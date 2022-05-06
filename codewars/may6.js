9675aac59a6ae3947989c670aa9bdac770c1d4df
Ngando Ntone <ngandontone33@gmail.com>
2022-05-06 15:40:15 -0500

    may6 2nd problem

--- codewars/may6.js
+++ codewars/may6.js
@@ -1,8 +1,13 @@
-1; //
+//1
 function repeatStr(n, s) {
   let string = "";
   for (let i = 1; i <= n; i++) {
     string += s;
   }
   return string;
+}
+//2
+function find_average(array) {
+  // your code here
+  return array.length > 0 ? array.reduce((x, y) => x + y) / array.length : 0;
 }
