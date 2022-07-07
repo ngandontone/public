a436206b7936adc20fdcaed529d3584c7eafaa7c
Ngando Ntone <ngandontone33@gmail.com>
2022-07-07 10:38:50 -0500

    missing number

---
+++ leetcodepractice/missingnumber.py
@@ -0,0 +1,8 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = len(nums)
+        
+        for i in range(len(nums)):
+            res ^= nums[i] ^ i
+            
+        return res
